81272dc75f236c35bd9e17c7ad979dfb96a49fb1
Conti <oliver_meier@hotmail.com>
2021-01-16 04:23:34 +0100

    version bump

--- src/Main.cs
+++ src/Main.cs
@@ -51,7 +51,7 @@ namespace AutoLightshow
             public const string Name = "AutoLightshow";  // Name of the Mod.  (MUST BE SET)
             public const string Author = "Continuum"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+            public const string Version = "1.5.0"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
         }
         
